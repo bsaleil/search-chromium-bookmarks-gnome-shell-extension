ff56d2c7b20c9e2b7a6770aec209808bdfdae614
mathematicalcoffee <mathematical.coffee@gmail.com>
2012-11-08 11:49:25 +1000

    various bugfixes; got working for google-chrome (need to test with both installed). Working on 3.2. Finish testing on 3.4 and 3.6 then merge to stable.

--- extension.js
+++ extension.js
@@ -29,6 +29,10 @@ const BookmarkPaths = {
             'google-chrome/Default/Bookmarks'
         ])
 };
+// GNOME 3.2 and 3.4: FileMonitorFlags. 3.6 (TODO: CHECK) MonitorFlags
+if (!Gio.FileMonitorFlags) {
+    Gio.FileMonitorFlags = Gio.MonitorFlags;
+}
 
 // Lock the instance of the search provider
 var ChromiumInst = null;
@@ -52,13 +56,13 @@ ChromiumBookmarksSearch.prototype = {
             if (!BookmarkPaths.hasOwnProperty(appn)) {
                 continue;
             }
-            let app = appSys.initial_search([app]);
+            let app = appSys.initial_search([appn]);
             if (!app.length) {
-                return;
+                continue;
             }
             let bookmarksPath = BookmarkPaths[appn];
             let file = Gio.file_new_for_path(bookmarksPath),
-                monitor = file.monitor(Gio.MonitorFlags.NONE, null);
+                monitor = file.monitor(Gio.FileMonitorFlags.NONE, null);
             monitor.connect('changed', Lang.bind(this, function () {
                 this._readBookmarks(appn);
             }));
@@ -90,7 +94,7 @@ ChromiumBookmarksSearch.prototype = {
         // delete old bookmarks for this app
         for (let bm in this._bookmarks) {
             if (!this._bookmarks.hasOwnProperty(bm)) {
-                return;
+                continue;
             }
             if (bm.substr(0, appn.length + DELIM.length) === (appn + DELIM)) {
                 delete this._bookmarks[bm];
@@ -113,7 +117,8 @@ ChromiumBookmarksSearch.prototype = {
                 this._bookmarks[id] = {
                     id: id,
                     name: bm.name,
-                    match_against: (bm.name + bm.url).toLowerCase().replace(/\s/g, ''),
+                    url_lower: bm.url.toLowerCase(),
+                    name_lower: bm.name.toLowerCase(),
                     url: bm.url,
                     app: this._apps[appn].app,
                     createIcon: Lang.bind(this, function (size) {
@@ -144,20 +149,62 @@ ChromiumBookmarksSearch.prototype = {
         result.app.launch(global.get_current_time(), [result.url], -1);
 	},
 
-	getInitialResultSet: function (terms) {
-		let results = [];
-        let pattern = terms.join('').replace(/\s/g, '').toLowerCase();
-        for (let id in this._bookmarks) {
-            if (!this._bookmarks.hasOwnProperty(id)) {
+    _searchBookmarks: function (bookmarks, terms) {
+        /* Items where term matches multiple criteria (e.g. name and URL)
+         * before single matches.
+         * Items which match on a prefix before non-prefix substring matches.
+         */
+        let prefixMatch = [];
+        let otherMatch = [];
+        for (let id = 0; id < bookmarks.length; ++id) {
+            let bm = this._bookmarks[bookmarks[id]];
+
+            // you must match *all* terms.
+            let matches = false,
+                prefix = false;
+            for (let i = 0; i < terms.length; ++i) {
+                let urlMatch = bm.url_lower.indexOf(terms[i]),
+                    nameMatch = bm.name_lower.indexOf(terms[i]);
+                if (urlMatch === -1 && nameMatch === -1) {
+                    matches = false;
+                    break;
+                }
+                matches = true;
+                if (urlMatch === 0 || nameMatch === 0) {
+                    prefix = true;
+                }
+            }
+            if (!matches) {
                 continue;
             }
-            let bm = this._bookmarks[id];
-
-			// Search pattern into lowercase name + url
-			if (bm.match_against.indexOf(pattern) > -1) {
-				results.push(bm);
+            if (prefix) {
+                prefixMatch.push(bm);
+            } else {
+                otherMatch.push(bm);
             }
 		}
+        // sort by name.
+        // TODO: should really sort by "relevance"/score the prefix
+        // and title a little higher than a url or substring match?
+        prefixMatch.sort(function (bm1, bm2) {
+            return bm1.name > bm2.name;
+        });
+        otherMatch.sort(function (bm1, bm2) {
+            return bm1.name > bm2.name;
+        });
+        let results = prefixMatch.concat(otherMatch).map(function (bm) {
+            return bm.id;
+        });
+        return results;
+    },
+
+    // TODO: make this async to speed things up?
+    // On GNOME 3.2: getInitialResultSet should be fast, use this.addItems for
+    // async.
+    // On GNOME 3.4: there appears to be *Async methods that user pushResults
+    // On GNOME 3.6: just use .pushResults for everything.
+	getInitialResultSet: function (terms) {
+        let results = this._searchBookmarks(Object.keys(this._bookmarks), terms);
         if (ShellVersion[1] >= 6) {
             this.searchSystem.pushResults(this, results);
         }
@@ -165,7 +212,11 @@ ChromiumBookmarksSearch.prototype = {
 	},
 
 	getSubsearchResultSet: function (previousResults, terms) {
-		return this.getInitialResultSet(terms);
+        let results = this._searchBookmarks(previousResults, terms);
+        if (ShellVersion[1] >= 6) {
+            this.searchSystem.pushResults(this, results);
+        }
+		return results;
 	},
 
     destroy: function () {
@@ -184,16 +235,14 @@ function init() {
 }
 
 function enable() {
-	if (ChromiumInst == null)
-	{
+	if (ChromiumInst == null) {
 		ChromiumInst = new ChromiumBookmarksSearch();
 		Main.overview.addSearchProvider(ChromiumInst);
 	}
 }
 
 function disable() {
-	if (ChromiumInst != null)
-	{
+	if (ChromiumInst != null) {
 		Main.overview.removeSearchProvider(ChromiumInst);
 		ChromiumInst.destroy();
 		ChromiumInst = null;
